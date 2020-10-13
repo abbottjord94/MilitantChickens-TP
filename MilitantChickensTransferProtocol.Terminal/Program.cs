6532b4c394921c77db7e07155bd2c3923479523e
Jordan Abbott <abbottjord94@gmail.com>
2020-10-13 13:47:04 -0700

    Add files via upload

---
+++ MilitantChickensTransferProtocol.Terminal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MilitantChickensTransferProtocol.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TcpConnectionClass.testFunction();
+        }
+    }
+}
---
+++ MilitantChickensTransferProtocol.Terminal/TcpConnectionClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitantChickensTransferProtocol.Terminal
+{
+    class TcpConnectionClass
+    {
+        public static void testFunction()
+        {
+            Console.WriteLine("test function from TcpConnectionClass");
+        }
+    }
+}
