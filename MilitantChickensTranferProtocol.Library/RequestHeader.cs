d3a58349a42cfb85fb1b0504e5e545203501d71c
pab15 <54556870+pab15@users.noreply.github.com>
2020-10-20 13:40:26 -0700

    Very Rough Outline Of The Request Header Base Class

---
+++ MilitantChickensTranferProtocol.Library/RequestHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MilitantChickensTranferProtocol.Library
+{
+    public class RequestHeader
+    {
+        public int requestCode { get; set; }
+        public string filePath { get; set; }
+        public RequestHeader()
+        {
+
+        }
+        public RequestHeader (int _requestCode, string _filePath)
+        {
+            requestCode = _requestCode;
+            filePath = _filePath;
+        }
+    }
+}
