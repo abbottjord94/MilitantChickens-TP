6f80eabc89d28f707c0250d00cef6fdc93cd31ba
pab15 <54556870+pab15@users.noreply.github.com>
2020-10-20 13:54:06 -0700

    Draft Of Basic Extended Classes

---
+++ MilitantChickensTranferProtocol.Library/PostRequestHeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitantChickensTranferProtocol.Library
+{
+    class PostRequestHeader : RequestHeader
+    {
+        byte[] data { get; set; }
+        PostRequestHeader(string _filePath, byte[] _data)
+        {
+            requestCode = 1;
+            filePath = _filePath;
+            data = _data;
+        }
+    }
+}
