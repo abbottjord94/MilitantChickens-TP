34f57966119b72af2ce289218cbc83d445c78734
Brian B <70234974+Staple0@users.noreply.github.com>
2020-10-22 14:19:53 -0700

    added ability to send message from client to server

--- MilitantChickensTransferProtocol.Terminal/Program.cs
+++ MilitantChickensTransferProtocol.Terminal/Program.cs
@@ -1,13 +1,55 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MilitantChickensTransferProtocol.Terminal
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Connect(String server, String message)
         {
-            Console.WriteLine("Hello World!");
-            TcpConnectionClass.testFunction();
+            try 
+            { 
+                Int32 clientPort = 9002;
+                TcpClient client = new TcpClient(server, clientPort);
+
+
+                BufferedStream stream = new BufferedStream(client.GetStream());
+
+                BinaryWriter writer = new BinaryWriter(stream);
+
+                Byte[] msg = System.Text.Encoding.UTF8.GetBytes(message);
+
+                stream.Write(msg);
+
+                stream.Flush();
+
+
+            }
+            catch
+            {
+                Exception e;
+            }
         }
+
+        public static void Main()
+        {
+            Console.WriteLine("Establishing connection to server");
+            try
+            {
+                Connect("10.0.0.1", "testing client connection");
+            }
+            catch
+            {
+                Console.WriteLine("could not establish connection");
+            }
+            
+
+        }
+
+
+
     }
 }
