ff1d8139c8509647742b126e5eff5d6855fe3fab
pab15 <54556870+pab15@users.noreply.github.com>
2020-10-22 19:59:23 -0700

    Attempt to Work Around Merge Conflicts

---
+++ MilitantChickensTranferProtocol.Library/RequestReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitantChickensTranferProtocol.Library
+{
+    class RequestReader
+    {
+        public string rawHeader { get; set; }
+        public RequestHeader packet { get; set; }
+        public RequestReader()
+        {
+
+        }
+        // Constructor that builds a reader from a byte[], plug and play with server
+        public RequestReader(byte[] _rawHeader)
+        {
+            rawHeader = Encoding.UTF8.GetString(_rawHeader);
+            // Separate each line (Goal is to only grab line #1 -- response code
+            string[] dataHeadSplit = rawHeader.Split("\n");
+            try
+            {
+                // Take the Code:<reqcode> and split the code off. Also verify its an int
+                // Using try/catch
+                int reqCode = Int32.Parse(dataHeadSplit[0].Split(":")[1]);
+                if (reqCode == 0)
+                {
+                    //TODO:
+                    // Parse Data and add it to constructor.
+                    string filePath = dataHeadSplit[1].Split(":")[1];
+                    // Since we used class inheritance, we can do this:
+                    packet = new GetRequestHeader(filePath);
+                }
+                if (reqCode == 1)
+                {
+                    //TODO:
+                    // Parse Data and add it to constructor.
+
+                    // Since we used class inheritance, we can do this:
+                    packet = new PostRequestHeader();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
