e83a1718d6235691aa8cce0a13af497567ae70e9
pab15 <54556870+pab15@users.noreply.github.com>
2020-10-28 20:12:22 -0700

    Post requests work, but error handling is bad

--- MilitantChickensTransferProtocol.GUIClientCore/Forms/SendForm.Designer.cs
+++ MilitantChickensTransferProtocol.GUIClientCore/Forms/SendForm.Designer.cs
@@ -19,6 +19,9 @@
             }
             base.Dispose(disposing);
         }
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Label messageBox;
 
         #region Windows Form Designer generated code
 
@@ -28,10 +31,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.Location = new System.Drawing.Point(145, 116);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.Size = new System.Drawing.Size(206, 20);
+            this.filePathBox.TabIndex = 0;
+            // 
+            // requestButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(205, 166);
+            this.sendButton.Name = "requestButton";
+            this.sendButton.Size = new System.Drawing.Size(97, 31);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "Upload File";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageBox
+            // 
+            this.Controls.Add(this.messageBox);
+            this.messageBox.AutoSize = true;
+            this.messageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageBox.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.messageBox.Location = new System.Drawing.Point(141, 223);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(174, 20);
+            this.messageBox.TabIndex = 2;
+            this.messageBox.Text = "Fuck You Visual Studio";
+            // ReceiveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(22)))), ((int)(((byte)(37)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SendForm";
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.filePathBox);
+            this.Name = "ReceiveForm";
+            this.Text = "ReceiveForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
